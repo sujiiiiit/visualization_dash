c4b55096b256e5784f10208b76bc3deb3343954d
Sujit Dwivedi <dwiwedisv@rknec.edu>
2024-08-01 12:52:27 +0530

    new

--- src/components/Sections/ArticleList/articleList.tsx
+++ src/components/Sections/ArticleList/articleList.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { fetchArticles } from "@/lib/service";
+import { fetchArticles, deleteMultipleRecords, deleteRecordById } from "@/lib/service";
 import { DataType } from "@/lib/types";
 import {
   sectors,
@@ -40,7 +40,21 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { PaginationDemo } from "@/components/Sections/ArticleList/pagination"; // Adjust import path as needed
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { DeleteIcon } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const ArticleList: React.FC = () => {
   const [articles, setArticles] = useState<DataType[]>([]);
@@ -60,45 +74,8 @@ const ArticleList: React.FC = () => {
     country: "",
   });
   const [selectedIds, setSelectedIds] = useState<Set<string>>(
-    new Set(JSON.parse(localStorage.getItem('selectedIds') || '[]'))
+    new Set(JSON.parse(localStorage.getItem("selectedIds") || "[]"))
   );
-
-  const handleFilterChange = (
-    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
-  ) => {
-    setFilters({
-      ...filters,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSelectChange = (name: string, value: string) => {
-    setFilters({
-      ...filters,
-      [name]: value === "all" ? "" : value,
-    });
-  };
-
-  const handleCheckboxChange = (id: string, checked: boolean) => {
-    const updatedSelectedIds = new Set(selectedIds);
-    console.log(updatedSelectedIds)
-    if (checked) {
-      updatedSelectedIds.add(id);
-    } else {
-      updatedSelectedIds.delete(id);
-    }
-    setSelectedIds(updatedSelectedIds);
-    localStorage.setItem('selectedIds', JSON.stringify(Array.from(updatedSelectedIds)));
-  };
-
-  const handleHeaderCheckboxChange = (checked: boolean) => {
-    const updatedSelectedIds = new Set<string>();
-    if (checked) {
-      articles.forEach(article => updatedSelectedIds.add(article._id.toString()));
-    }
-    setSelectedIds(updatedSelectedIds);
-    localStorage.setItem('selectedIds', JSON.stringify(Array.from(updatedSelectedIds)));
-  };
 
   useEffect(() => {
     const loadArticles = async () => {
@@ -121,13 +98,99 @@ const ArticleList: React.FC = () => {
   }, [currentPage, filters]);
 
   useEffect(() => {
-    // Sync selected rows with new page data
-    const updatedSelectedIds = new Set<string>(JSON.parse(localStorage.getItem('selectedIds') || '[]'));
-    setSelectedIds(updatedSelectedIds);
+    // Update selected IDs based on stored IDs and current articles
+    const storedIds = JSON.parse(localStorage.getItem("selectedIds") || "[]");
+    const updatedSelectedIds = new Set(storedIds);
+    setSelectedIds(
+      (prevIds) =>
+        new Set(
+          [...prevIds].filter(
+            (id) =>
+              articles.some((article) => article._id.toString() === id) ||
+              updatedSelectedIds.has(id)
+          )
+        )
+    );
   }, [articles]);
 
+  const handleFilterChange = (
+    e: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>
+  ) => {
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSelectChange = (name: string, value: string) => {
+    setFilters({
+      ...filters,
+      [name]: value === "all" ? "" : value,
+    });
+  };
+
+  const handleCheckboxChange = (id: string, checked: boolean) => {
+    const updatedSelectedIds = new Set(selectedIds);
+    if (checked) {
+      updatedSelectedIds.add(id);
+    } else {
+      updatedSelectedIds.delete(id);
+    }
+    setSelectedIds(updatedSelectedIds);
+    localStorage.setItem(
+      "selectedIds",
+      JSON.stringify(Array.from(updatedSelectedIds))
+    );
+    console.log("Selected IDs:", Array.from(updatedSelectedIds)); // Log the updated IDs
+  };
+
+  const handleHeaderCheckboxChange = (checked: boolean) => {
+    const updatedSelectedIds = new Set<string>();
+    if (checked) {
+      articles.forEach((article) =>
+        updatedSelectedIds.add(article._id.toString())
+      );
+    }
+    setSelectedIds(updatedSelectedIds);
+    localStorage.setItem(
+      "selectedIds",
+      JSON.stringify(Array.from(updatedSelectedIds))
+    );
+  };
+
+  const handleDeleteSelected = async () => {
+    try {
+      const idsToDelete = Array.from(selectedIds);
+      await deleteMultipleRecords(idsToDelete);
+      setArticles((prevArticles) =>
+        prevArticles.filter((article) => !selectedIds.has(article._id.toString()))
+      );
+      setSelectedIds(new Set());
+      localStorage.setItem("selectedIds", JSON.stringify([]));
+    } catch (error) {
+      console.error("Error deleting selected records:", error);
+    }
+  };
+
+  const handleDeleteSingle = async (id: string) => {
+    try {
+      await deleteRecordById(id);
+      setArticles((prevArticles) =>
+        prevArticles.filter((article) => article._id.toString() !== id)
+      );
+      setSelectedIds((prevIds) => {
+        const updatedIds = new Set(prevIds);
+        updatedIds.delete(id);
+        localStorage.setItem("selectedIds", JSON.stringify(Array.from(updatedIds)));
+        return updatedIds;
+      });
+    } catch (error) {
+      console.error("Error deleting record:", error);
+    }
+  };
+
   return (
-    <div className="p-2 sm:p-5 max-w-screen-lg">
+    <div className="p-2 sm:p-5 max-w-screen-lg m-auto">
       <div className="mb-4 flex flex-wrap sm:justify-between gap-2">
         <Input
           type="text"
@@ -137,175 +200,133 @@ const ArticleList: React.FC = () => {
           onChange={handleFilterChange}
           className="w-full"
         />
-        <Select
-          onValueChange={(value) => handleSelectChange("sector", value)}
-          value={filters.sector}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="Sectors" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All Sectors</SelectItem>
-              {sectors.map((sector) => (
-                <SelectItem key={sector} value={sector}>
-                  {sector}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Select
-          onValueChange={(value) => handleSelectChange("region", value)}
-          value={filters.region}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="Regions" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All Regions</SelectItem>
-              {regions.map((region) => (
-                <SelectItem key={region} value={region}>
-                  {region}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Select
-          onValueChange={(value) => handleSelectChange("end_year", value)}
-          value={filters.end_year}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="Years" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All Years</SelectItem>
-              {endYears.map((year) => (
-                <SelectItem key={year} value={String(year)}>
-                  {year}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Select
-          onValueChange={(value) => handleSelectChange("topic", value)}
-          value={filters.topic}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="Topics" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All Topics</SelectItem>
-              {topics.map((topic) => (
-                <SelectItem key={topic} value={topic}>
-                  {topic}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Select
-          onValueChange={(value) => handleSelectChange("pestle", value)}
-          value={filters.pestle}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="PESTLE" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All PESTLE</SelectItem>
-              {pests.map((pest) => (
-                <SelectItem key={pest} value={pest}>
-                  {pest}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Select
-          onValueChange={(value) => handleSelectChange("source", value)}
-          value={filters.source}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="Sources" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All Sources</SelectItem>
-              {sources.map((source) => (
-                <SelectItem key={source} value={source}>
-                  {source}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-        <Select
-          onValueChange={(value) => handleSelectChange("country", value)}
-          value={filters.country}
-        >
-          <SelectTrigger className="w-fit md:w-[132px] h-8">
-            <SelectValue placeholder="Countries" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectGroup>
-              <SelectItem value="all">All Countries</SelectItem>
-              {countries.map((country) => (
-                <SelectItem key={country} value={country}>
-                  {country}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
+        {/* Filter dropdowns */}
+        {[
+          { name: "sector", placeholder: "Sectors", options: sectors },
+          { name: "region", placeholder: "Regions", options: regions },
+          {
+            name: "end_year",
+            placeholder: "Years",
+            options: endYears.map((year) => String(year)),
+          },
+          { name: "topic", placeholder: "Topics", options: topics },
+          { name: "pestle", placeholder: "PESTLE", options: pests },
+          { name: "source", placeholder: "Sources", options: sources },
+          { name: "country", placeholder: "Countries", options: countries },
+        ].map(({ name, placeholder, options }) => (
+          <Select
+            key={name}
+            onValueChange={(value) => handleSelectChange(name, value)}
+            value={filters[name]}
+          >
+            <SelectTrigger className="w-fit md:w-[132px] h-8">
+              <SelectValue placeholder={placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="all">All {placeholder}</SelectItem>
+                {options.map((option) => (
+                  <SelectItem key={option} value={option}>
+                    {option}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        ))}
       </div>
 
       {loading && <p>Loading...</p>}
       {error && <p className="text-red-500">Error: {error}</p>}
       {!loading && !error && (
         <div>
-          <div className="text-sm px-4 py-2 border border-b-0 rounded-t-lg">Selected {selectedIds.size} out of {totalRecords} rows</div>
-          <Table className="border rounded-lg ">
+          <div className="text-sm px-4 py-2 border border-b-0 rounded-t-lg flex justify-between items-center">
+            <div>Selected {selectedIds.size} out of {totalRecords} rows</div>
+            <div>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button size={"icon"} variant={"ghost"}>
+                    <DeleteIcon />
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete the selected records.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleDeleteSelected}>
+                      Continue
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+            </div>
+          </div>
+          <Table className="border rounded-lg">
             <TableHeader>
               <TableRow>
                 <TableHead className="px-4">
                   <Checkbox
-                    checked={articles.every(article => selectedIds.has(String(article._id)))}
-                    onCheckedChange={(checked) => handleHeaderCheckboxChange(Boolean(checked))}
+                    checked={articles.every((article) =>
+                      selectedIds.has(String(article._id))
+                    )}
+                    onCheckedChange={(checked) =>
+                      handleHeaderCheckboxChange(Boolean(checked))
+                    }
                   />
                 </TableHead>
                 <TableHead className="px-4">Title</TableHead>
-                <TableHead className="hidden sm:flex">Published</TableHead>
+                <TableHead className="hidden sm:flex items-center">
+                  Published
+                </TableHead>
                 <TableHead>More</TableHead>
                 {/* Add other table headers as needed */}
               </TableRow>
             </TableHeader>
             <TableBody>
               {articles.map((article) => (
-                <TableRow key={article._id} className={`${selectedIds.has(String(article._id))?"bg-muted":''}`}>
+                <TableRow
+                  key={article._id}
+                  className={`${
+                    selectedIds.has(String(article._id)) ? "bg-muted" : ""
+                  }`}
+                >
                   <TableCell className="px-4">
                     <Checkbox
                       checked={selectedIds.has(String(article._id))}
-                      onCheckedChange={(checked) => handleCheckboxChange(String(article._id), Boolean(checked))}
+                      onCheckedChange={(checked) =>
+                        handleCheckboxChange(
+                          String(article._id),
+                          Boolean(checked)
+                        )
+                      }
                     />
                   </TableCell>
-                  <TableCell className="px-4 text-sm">{article.title}</TableCell>
-                  <TableCell className="text-nowrap hidden sm:flex">{convertDateFormat(article.published)}</TableCell>
+                  <TableCell className="px-4 text-sm">
+                    <div className="flex flex-col gap-1">
+                    <span>{article.title}</span>
+                    <span className="text-xs sm:hidden">Published : {convertDateFormat(article.published)}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-nowrap hidden sm:flex">
+                    {convertDateFormat(article.published)}
+                  </TableCell>
                   <TableCell className="px-4">
                     <DropdownMenu>
                       <DropdownMenuTrigger className="w-8 h-8 hover:bg-muted flex justify-center items-center">
-                          <DotsHorizontalIcon className="h-4 w-4" />
+                        <DotsHorizontalIcon className="h-4 w-4" />
                       </DropdownMenuTrigger>
                       <DropdownMenuContent>
                         <DropdownMenuLabel>More Actions</DropdownMenuLabel>
                         <DropdownMenuSeparator />
                         <DropdownMenuItem>View/Edit</DropdownMenuItem>
-                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                        <DropdownMenuItem onClick={() => handleDeleteSingle(article._id.toString())}>
+                          Delete
+                        </DropdownMenuItem>
                       </DropdownMenuContent>
                     </DropdownMenu>
                   </TableCell>
@@ -314,15 +335,12 @@ const ArticleList: React.FC = () => {
             </TableBody>
           </Table>
           <div className="text-sm px-4 py-2 border border-t-0 rounded-b-lg">
-          <PaginationDemo
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={setCurrentPage}
-          />
+            <PaginationDemo
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={setCurrentPage}
+            />
           </div>
-
-          
-         
         </div>
       )}
     </div>
--- src/lib/service.ts
+++ src/lib/service.ts
@@ -2,15 +2,15 @@
 import axios from "axios";
 import { DataType } from "./types";
 
-const API_URL =  "http://localhost:5000/api";
+const API_URL = "http://localhost:5000/api";
 // const API_URL =  "https://blackcofferapi.vercel.app/api";
 
 export const fetchArticles = async (
   page: number = 1,
   filters: any = {}
-): Promise<{ articles: DataType[]; totalPages: number; currentPage: number;totalRecords:number; }> => {
+): Promise<{ articles: DataType[]; totalPages: number; currentPage: number; totalRecords: number; }> => {
   try {
-    const response = await axios.get<{ articles: DataType[]; totalPages: number; currentPage: number;totalRecords:number; }>(
+    const response = await axios.get<{ articles: DataType[]; totalPages: number; currentPage: number; totalRecords: number; }>(
       `${API_URL}/search`,
       {
         params: {
@@ -30,5 +30,43 @@ export const fetchArticles = async (
       console.error("Fetch articles error:", error);
     }
     throw new Error("Failed to fetch articles. Please try again.");
+  }
+};
+
+// Add the delete multiple records service
+export const deleteMultipleRecords = async (ids: string[]): Promise<{ message: string; }> => {
+  try {
+    const response = await axios.delete<{ message: string; }>(`${API_URL}/search/multiple`, {
+      data: { ids }
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Delete multiple records error:",
+        error.response?.data || error.message
+      );
+    } else {
+      console.error("Delete multiple records error:", error);
+    }
+    throw new Error("Failed to delete records. Please try again.");
+  }
+};
+
+// Add the delete single record service
+export const deleteRecordById = async (id: string): Promise<{ message: string; }> => {
+  try {
+    const response = await axios.delete<{ message: string; }>(`${API_URL}/search/${id}`);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(
+        "Delete record error:",
+        error.response?.data || error.message
+      );
+    } else {
+      console.error("Delete record error:", error);
+    }
+    throw new Error("Failed to delete record. Please try again.");
   }
 };
